c61b9686f7859c28b106c84d7af1ddbb494c72ff
Jay Kudva <jaykudva@gmail.com>
2020-07-22 19:09:59 -0700

    [FIX] changed index.js routing to redirect to index.html [ADD] added bonfire fire in place of the laughing crying emoji

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', function(req, res) {
-    res.redirect('/opencv.html')
+    res.redirect('/index.html')
 })
 
 app.use(express.static(__dirname + '/static'))
